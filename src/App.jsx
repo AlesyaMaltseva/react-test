6c30aad8491b97ed04de4154c5025ad0bd67f2da
AlesyaMaltseva <alesya.maltseva@gmail.com>
2025-08-18 13:31:22 +0300

    без build

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>Обновляем код</div>
+      <div>Вставляем новый код</div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
