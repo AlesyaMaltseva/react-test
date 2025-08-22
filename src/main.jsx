7eb78784f66669f123519c9686cb1aaf0425ebc4
AlesyaMaltseva <alesya.maltseva@gmail.com>
2025-08-22 13:54:46 +0300

    gsap, scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './react-style.scss'
+import { gsap } from "gsap"
 // import App from './App.jsx'
+
+
 import Lesson from './Lesson.jsx'
 
 createRoot(document.getElementById('root')).render(
